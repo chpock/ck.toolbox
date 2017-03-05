9a900e9a710ad9b72b141aea3cba171fd3908ab9
Konstantin Kushnir <chpock@gmail.com>
2017-03-05 03:15:26 +0200

    Added option --cmd <command> to exec command on hosts

--- personal/remoteexec.cmd
+++ personal/remoteexec.cmd
@@ -1,37 +1,83 @@
+::if no { -*- mode: tcl; tab-width: 4; -*-
+:: vim: set syntax=tcl shiftwidth=4:
+@remotetool --script "%~f0" %*
+@goto :eof
+}
 
-namespace eval ::exec_shell_script {
+namespace eval ::sh_script {
 
     variable shell_script
+    variable shell_command
 
     proc init { } {
         variable shell_script
+        variable shell_command
 
         if { ![llength $::argv] } {
-            puts "No shell script to execute."
+            puts "Error: no shell script to execute."
             exit 1
         }
 
-        set shell_script [file normalize [lindex $::argv 0]]
+        set shell_script [lindex $::argv 0]
 
-        if { ![file exists $shell_script] } {
-            puts "Shell script file not found: [lindex $::argv 0]"
-            exit 1
+        if { \
+            [string match -nocase "-cmd*" $shell_script] || \
+            [string match -nocase "--cmd*" $shell_script] || \
+            [string match -nocase "-command*" $shell_script] || \
+            [string match -nocase "--command*" $shell_script] \
+        } {
+
+            unset shell_script
+
+            if { [llength $::argv] < 2 } {
+                puts "Error: no command specified in command line"
+                exit 1
+            }
+
+            set shell_command [lindex $::argv 1]
+
+        } {
+
+            if { ![file exists $shell_script] } {
+                puts "Error: shell script file not found: [lindex $::argv 0]"
+                exit 1
+            }
+
         }
 
     }
 
     proc run { host config } {
         variable shell_script
+        variable shell_command
 
         if { ![check_ssh_connect $config] } {
             puts -nonewline " OFFLINE"
         } {
 
-            if { [catch { exec -- {*}[auto_execok plink] -m [file nativename $shell_script] [dict get $config UserName]@[dict get $config HostName] << "y\ny" 2>@1 } err] } {
+            set cmd [auto_execok plink]
+
+            lappend cmd [dict get $config UserName]@[dict get $config HostName]
+
+            if { [dict exists $config Password] && [dict get $config Password] ne "" } {
+                lappend cmd -pw [dict get $config Password]
+            }
+
+            if { [info exists shell_script] } {
+                lappend cmd -m [file nativename $shell_script]
+            } {
+                lappend cmd $shell_command
+            }
+
+            if { [catch { exec -- {*}$cmd  << "y\ny" 2>@1 } err] } {
                 puts -nonewline " ERROR: "
             } {
                 puts -nonewline " OK :"
             }
+
+            regsub {The server's \w+? key fingerprint is:\n.+?\(y/n\)\s+} $err {} err
+            regsub {The first key-exchange algorithm supported by the server.+?\(y/n\)\s+} $err {} err
+            regsub {The server's host key is not cached in the registry\. You.+?think it is\.\s+} $err {} err
 
             puts -nonewline $err
 
